7d74189b2d7dd301f5c164fb6208405a1986d268
Alexey Lapin <alexey-lapin@protonmail.com>
2021-04-28 20:19:41 +0300

    refactor repositories

--- src/main/java/jmsmock/domain/repository/ReceiverConfigRepository.java
+++ src/main/java/jmsmock/domain/repository/ReceiverConfigRepository.java
@@ -1,11 +1,9 @@
 package jmsmock.domain.repository;
 
 import jmsmock.domain.model.ReceiverConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface ReceiverConfigRepository {
 
--- src/main/java/jmsmock/domain/repository/SenderConfigRepository.java
+++ src/main/java/jmsmock/domain/repository/SenderConfigRepository.java
@@ -1,11 +1,9 @@
 package jmsmock.domain.repository;
 
 import jmsmock.domain.model.SenderConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface SenderConfigRepository {
 
