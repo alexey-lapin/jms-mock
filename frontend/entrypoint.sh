8f1ad42c18c521b42074676269ac7242315a7ce9
Alexey Lapin <alexey-lapin@protonmail.com>
2021-06-02 21:13:58 +0300

    fix frontend env variables

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-: "${UI_PUBLIC_PATH:=''}"
-: "${UI_API_BASE_PATH:='/api'}"
+: "${UI_PUBLIC_PATH:=}"
+: "${UI_API_BASE_PATH:=$UI_PUBLIC_PATH/api}"
 
 sed -i "s@<PUBLIC_PATH_REPLACE>@$UI_PUBLIC_PATH@g" /usr/share/nginx/html/index.html
 sed -i "s@<PUBLIC_PATH_REPLACE>@$UI_PUBLIC_PATH@g" /usr/share/nginx/html/js/app.*.js
